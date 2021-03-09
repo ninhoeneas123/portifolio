6fa61c3cf2c660e5df76495dd547a07976b91d54
Jose eneas <ninho.serafim95@gmail.com>
2021-03-09 19:07:54 -0300

    projeto criado

---
+++ script.js
@@ -0,0 +1,9 @@
+var hamburguer = document.querySelector(".hamburguer"); 
+
+hamburguer.addEventListener("click", function(){
+
+var sidebar = document.querySelector(".sidebar").classList.toggle("show-menu");
+
+
+
+});
